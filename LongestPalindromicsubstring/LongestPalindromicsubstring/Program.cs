ff951e1fe754a32d9d61546c1bec8d3a1ec5a988
schebiwot <s.chebiwot@bigmetal.me>
2020-09-08 14:59:00 +0300

    Time complexity -O(n^2) space complexity o(n)

---
+++ LongestPalindromicsubstring/LongestPalindromicsubstring/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace LongestPalindromicsubstring
+{
+    class Program
+    {
+
+        //Time complexity-O(n2);
+       // space(o(n))
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindLongestPalindrome("toik"));
+        }
+        public static string FindLongestPalindrome(string word)
+        {
+            string wordsubstring = "";
+            string LongestPalindrome = "";
+            string result = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < word.Length - i; j++)
+                {
+                    wordsubstring = word.Substring(i, j + 1);
+
+                  
+                    if (IsPalindrome(wordsubstring))
+                    {
+                        if (wordsubstring.Length > LongestPalindrome.Length)
+                        {
+                            LongestPalindrome = wordsubstring;
+                            result = LongestPalindrome;
+                        }
+                        else
+                        {
+                            result = "There is palindromic substring";
+                        }
+                    }
+
+
+                }
+            }
+
+
+            return result;
+
+        }
+
+   
+        public static bool IsPalindrome(string str)
+        {
+            char[] s = str.ToCharArray();
+            Array.Reverse(s);
+            string newstr = new string(s);
+            if (str == newstr)
+            {
+                return true;
+            }
+        
+
+            return false;
+        }
+
+    }
+
+    //Write a program that finds the longest palindromic substring of a given string.
+
+    //1001
+    //greek
+    //tone
+}
+   
---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibonacci
+{
+    //Write a function that computes the list of the first 100 Fibonacci numbers.
+    //    The first two Fibonacci numbers are 1 and 1. The n+1-st Fibonacci number can be computed by adding the n-th and the n-1-th Fibonacci number.
+    //    The first few are therefore 1, 1, 1+1=2, 1+2=3, 2+3=5, 3+5=8.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findFibonacci(100));
+        }
+        public static int findFibonacci(int n)
+        {
+
+            int fib = 0;
+            int fib1 = 1;
+
+
+            // List<int> store = new List<int>();
+            int nextelement = 0;
+
+            //store.Add(fib1);
+            for (int i = 1; i <= n; i++)
+            {
+                nextelement = fib + fib1;
+
+                fib = fib1;
+
+                fib1 = nextelement;
+
+                //store.Add(nextelement);
+
+                Console.WriteLine(nextelement);
+
+            }
+
+            return nextelement;
+        }
+           
+
+        
+    }
+}
